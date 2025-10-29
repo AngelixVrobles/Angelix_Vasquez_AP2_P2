68cb5442e9ce480799760ef180b058f6b7e9630e
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-10-29 19:32:37 -0400

    Refactor(build): Migrar plugins a `id()` y agregar KSP y Hilt
    Se actualiza el bloque de plugins en `build.gradle.kts` para utilizar la función `id()` en lugar de los alias de `libs.plugins`.
    
    Además, se agregan los plugins para KSP (`com.google.devtools.ksp`) y Hilt (`com.google.dagger.hilt.android`), estableciendo sus respectivas versiones.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file for Angelix_Vasquez_AP2_P2
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.7.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
 }

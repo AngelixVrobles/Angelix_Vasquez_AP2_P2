d2b42a85280d009478ff8009396abcd8f7e4f9c2
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-11-11 15:32:51 -0400

    Crear Deposito.kt

---
+++ app/src/main/java/com/example/angelix_vasquez_ap2_p2/domain/model/Deposito.kt
@@ -0,0 +1,9 @@
+package com.example.angelix_vasquez_ap2_p2.domain.model
+
+class Deposito (
+    val depositoId : Int,
+    val fecha : String,
+    val monto : Int,
+    val banco : String,
+    val concepto : String
+)

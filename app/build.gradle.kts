9153bd195664dd64f29d9a109f4d4267506c00e9
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-11-11 15:32:37 -0400

    Installar depedencias faltatantes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,9 +76,10 @@ dependencies {
 
     // Retrofit y OkHttp (API REST)
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
 
     // Hilt (inyección de dependencias)
     implementation("com.google.dagger:hilt-android:2.52")
--- app/src/main/java/com/example/angelix_vasquez_ap2_p2/App.kt
+++
@@ -1,7 +0,0 @@
-package com.example.angelix_vasquez_ap2_p2
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App : Application()

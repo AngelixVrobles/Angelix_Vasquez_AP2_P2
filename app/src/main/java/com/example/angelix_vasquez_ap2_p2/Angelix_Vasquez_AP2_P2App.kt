075b407ed45847353ee22009727e7a7d0803457b
Angelix Vasquez Robles <angelle120404@gmail.com>
2025-11-11 15:31:38 -0400

    Hilt

---
+++ app/src/main/java/com/example/angelix_vasquez_ap2_p2/Angelix_Vasquez_AP2_P2App.kt
@@ -0,0 +1,8 @@
+package com.example.angelix_vasquez_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Angelix_Vasquez_AP2_P2App : Application() {
+}
